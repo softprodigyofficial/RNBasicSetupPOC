eed1637e224a6cc5118a04164b577897491c4847
vaibhavsps1 <120564997+vaibhavsps1@users.noreply.github.com>
2023-08-02 19:31:00 +0530

    Revert "July sps"

--- src/redux/store.ts
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import rootReducer from "./reducer/index";
-import thunk from "redux-thunk";
-
-export const store = createStore(rootReducer, applyMiddleware(thunk));
-        
--- src/utils/images.ts
+++
@@ -1,4 +0,0 @@
-//Place all svg and png icon here
-export const BASE_PATH =
-  'https://raw.githubusercontent.com/AboutReact/sampleresource/master/';
-export const proileImage = 'react_logo.png';
